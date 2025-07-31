d44fa704852cd41ef6bdf1b597820f75cf38319d
satya <mssrinivas89@gmail.com>
2025-08-01 00:21:16 +0530

    Initial commit of new Java project

---
+++ CompleteReferenceJava/src/com/JavaTCRG/Example.java
@@ -0,0 +1,11 @@
+package com.JavaTCRG;
+
+public class Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a simple Java program.");
+
+	}
+
+}
---
+++ CompleteReferenceJava/src/com/JavaTCRG/Example2.java
@@ -0,0 +1,15 @@
+package com.JavaTCRG;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num;
+		num = 100;
+		System.out.println("This is num: "+ num);
+		num = num * 2;
+		System.out.println("\"The value of num * 2 is ");
+		System.out.println(num);
+	}
+
+}
